2d4fda7d79a19dd4cb1d1f75f7eb64c32f36d93d
rnbroggi <rnbroggi@gmail.com>
2021-08-24 13:01:43 -0300

    Agregado medialibrary, auditoria y softdelete a modelo de visita

--- app/Visita.php
+++ app/Visita.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Visita extends Model
+class Visita extends Model implements Auditable, HasMedia
 {
+    use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
+    use HasMediaTrait;
+    
     protected $table = 'visitas';
     protected $guarded = ['id'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at', 'fecha'];
